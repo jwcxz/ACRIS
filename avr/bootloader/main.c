56e20eeed9336ef9bd18e596dc7055993c4d4741
Joe Colosimo <jwc@jwcxz.com>
2011-06-01 20:50:24 -0400

    started bootloader

---
+++ avr/bootloader/main.c
@@ -0,0 +1,18 @@
+/************************************
+ * A C R I S   P R O J E C T        *
+ * LED Controller Bootloader        *
+ * http://jwcxz.com/projects/acris  *
+ *                                  *
+ * J. Colosimo - http://jwcxz.com/  *
+ *                                  *
+ * Main bootloader source file      *
+ ************************************/
+
+/*
+ * This bootloader provides the following features:
+ *  1. fast startup time (i.e. bootloader->application time)
+ *  2. downloading of program to device
+ *  3. set address of instrument
+ *  4. set the transmission baud rate (via the divisor)
+ *  5. program verification
+ */
