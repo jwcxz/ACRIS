d173171aa52fbe6727e9e9fcf7ffb63499fd0120
Joe Colosimo <jwc@jwcxz.com>
2011-09-09 12:35:25 -0400

    oh what the hell... phase offset is now changeable

--- sw/acris/plugins/huecycle.py
+++ sw/acris/plugins/huecycle.py
@@ -19,16 +19,19 @@ class Plugin(backend.plugin.Plugin):
         val = 1.0;
         hue = 0;
 
-        phase = [ 0, 45 ];
-
         if len(self.args) >= 1: maxv = int(self.args[0]);
         else:                   maxv = 90;
 
         if len(self.args) >= 2: timedelay = float(self.args[1]);
         else:                   timedelay = 0.05;
 
-        if len(self.args) >= 3: huestep = float(self.args[2]);
+        if len(self.args) >= 3: huestep = int(self.args[2]);
         else:                   huestep = 1;
+
+        if len(self.args) >= 4: phaseoff = int(self.args[3]);
+        else:                   phaseoff = 45;
+
+        phase = [ 0, phaseoff ];
 
         while self.enabled:
             rgb = [ [ int(maxv * i) for i in backend.utils.hsv2rgb(hue + x, sat, val) ] for x in phase ];
