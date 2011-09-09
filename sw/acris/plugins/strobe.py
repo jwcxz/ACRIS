16bb822543d61f0835df57339ccdd1af7a76243b
Joe Colosimo <jwc@jwcxz.com>
2011-09-09 12:30:33 -0400

    made brightness adjustable

--- sw/acris/plugins/strobe.py
+++ sw/acris/plugins/strobe.py
@@ -4,13 +4,18 @@ class Plugin(backend.plugin.Plugin):
     def run(self): 
         backend.plugin.Plugin.run(self);
 
-        if len(self.args) >= 1: offtime = float(self.args[0]);
+        if len(self.args) >= 1: brightness = self.args[0];
+        else:                   brightness = 200;
+
+        if len(self.args) >= 2: offtime = float(self.args[1]);
         else:                   offtime = .07
 
-        if len(self.args) >= 2: ontime = float(self.args[1]);
+        if len(self.args) >= 3: ontime = float(self.args[2]);
         else:                   ontime = .01
 
-        oncmd = [255, 220, 220, 220, 0, 0, 0, 220, 220, 220, 0, 0, 0, 220, 220, 220];
+        oncmd = [255, brightness, brightness, brightness, 0, 0, 0, brightness,
+                brightness, brightness, 0, 0, 0, brightness, brightness,
+                brightness];
         offcmd = [255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         while self.enabled:
