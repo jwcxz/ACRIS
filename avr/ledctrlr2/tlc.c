0082bb4fd7810141c075a1a3ad1d75d01abfde9f
Joe Colosimo <jwc@jwcxz.com>
2012-12-30 02:51:05 -0500

    inline pulse_xlat

--- avr/ledctrlr2/tlc.c
+++ avr/ledctrlr2/tlc.c
@@ -1,5 +1,12 @@
 #include "tlc.h"
 
+__inline__ void pulse_xlat(void) {
+    _ON(TLCTRL_PRT, XLAT_PIN);
+    _delay_us(XLATPD);
+    _OFF(TLCTRL_PRT, XLAT_PIN);
+    _delay_us(XLATPD);
+}
+
 void tlc_init(void) {
     uint8_t i;
 
@@ -17,16 +24,14 @@ void tlc_init(void) {
     SPCR = _BV(SPE) | _BV(MSTR);    // enable spi in master
     SPCR |= _BV(SPR1);              // speed register (don't set this too fast)
 
+    // initialize the shift register with all 0s
     for ( i=0 ; i<24*3 ; i++ ) {
         SPDR = 0;
         while ( !(SPSR & _BV(SPIF)) );
     }
 
     // pulse the latch
-    _ON(TLCTRL_PRT, XLAT_PIN);
-    _delay_us(XLATPD);
-    _OFF(TLCTRL_PRT, XLAT_PIN);
-    _delay_us(XLATPD);
+    pulse_xlat();
 
     // BLANK counter hits every 4096 GSCLK cycles
     TCCR1A = _BV(COM1B1);
@@ -57,10 +62,7 @@ void tlc_drive(void) {
     }
 
     // pulse the latch
-    _ON(TLCTRL_PRT, XLAT_PIN);
-    _delay_us(XLATPD);
-    _OFF(TLCTRL_PRT, XLAT_PIN);
-    _delay_us(XLATPD);
+    pulse_xlat();
 }
 
 /* HELPER FUNCTIONS */
