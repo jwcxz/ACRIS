b7f7dcde3202c0f958984273b2d8a53b26730f42
Joe Colosimo <jwc@jwcxz.com>
2012-12-30 04:06:07 -0500

    update full range test to experiment with more stuff

--- sw/serial/fullrangetest.py
+++ sw/serial/fullrangetest.py
@@ -4,8 +4,9 @@ import math, time, serial, sys
 
 SYNC=0x55;
 timestep = 0.01;
-huestep = 1;
-maxrange = 4095;
+huestep = 2;
+maxrange = 64;
+huephase=20;
 
 def convert(d):
     if d == SYNC: d += 1;
@@ -19,15 +20,22 @@ def hsv2rgb(h, s, v):
     t = v * (1.0 - ((1.0 - f) * s))
     return { 0: (v, t, p), 1: (q, v, p), 2: (p, v, t), 3: (p, q, v), 4: (t, p, v), 5: (v, p, q), }[hi]
 
+def wbyte(a, b):
+    return [ (a>>4)&0xFF , ((a&0xF) << 4) | ( (b>>8)&0xF ) , b&0xFF ];
+
 def send(rgb):
-    sendarr = [ 0x11, 0x80 ];
-    r,g,b = [ int(_) for _ in rgb ];
+    sendarr = [ 0x01, 0xFF ];
     
-    sendarr.append( r>>8 );
-    sendarr.append( r&0xFF );
-    sendarr.append( g>>4 );
-    sendarr.append( ((g&0xF)<<4) | b>>8 );
-    sendarr.append( b&0xFF );
+    sendarr.extend( wbyte(0, rgb[0][0])[1:3] );
+    sendarr.extend( wbyte(rgb[0][1], rgb[0][2]) );
+    sendarr.extend( wbyte(rgb[1][0], rgb[1][1]) );
+    sendarr.extend( wbyte(rgb[1][2], rgb[2][0]) );
+    sendarr.extend( wbyte(rgb[2][1], rgb[2][2]) );
+    sendarr.extend( wbyte(rgb[3][0], rgb[3][1]) );
+    sendarr.extend( wbyte(rgb[3][2], rgb[4][0]) );
+    sendarr.extend( wbyte(rgb[4][1], rgb[4][2]) );
+    
+    #print sendarr
     
     sendstr = chr(SYNC);
     for _ in sendarr:
@@ -40,12 +48,11 @@ serport = "/dev/ttyUSB0"
 cxn = serial.Serial(serport, 38400, parity=serial.PARITY_EVEN);
 print ":: opened", cxn.portstr;
 
-send( [ 0, 0, 0 ] );
-
 hue = 0;
 
 while True:
-    rgb = [ maxrange*_ for _ in hsv2rgb(hue, 1.0, 1.0) ];
+    rgb = [ [ int(maxrange*_) for _ in hsv2rgb(hue+offs, 1.0, 1.0) ] for offs in (0, huephase, 2*huephase, 3*huephase, 4*huephase) ];
+    #print rgb
     send(rgb);
     hue = (hue+huestep) % 360.;
     time.sleep(timestep);
