3f667a3f0a93865875c8ef21a03c684fd26e6df1
Joe Colosimo <jwc@jwcxz.com>
2013-10-14 01:28:01 -0700

    move EEPROM addresses to their own file

---
+++ avr/inc/eeprom_map.h
@@ -0,0 +1,13 @@
+#ifndef __EEPROM_H
+#define __EEPROM_H
+
+// ACRIS EEPROM address mapping
+
+// instrument address
+#define EEPROM_INST_ADDR    ((uint8_t*) 0)
+
+// wireless channel
+#define EEPROM_INST_CHAN    ((uint8_t*) 4)
+
+
+#endif
